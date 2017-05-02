076e55a579ad9bd43cbad4569a5a3620a9435c39
actuallycoding <15017484@myrp.edu.sg>
2017-05-02 12:20:20 +0800

    Added the second activity and the xml. only the XML has something though.

---
+++ app/src/main/java/android/myapplicationdev/com/p03_classjournal/SecondActivity.java
@@ -0,0 +1,14 @@
+package android.myapplicationdev.com.p03_classjournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
